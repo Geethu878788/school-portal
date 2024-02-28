c84410ba3fa6881aaaaed45c1437f9a5d18afec6
Geethu Saseendran <geethusaseendran866@gmail.com>
2024-02-28 11:47:02 -0500

    new folders

---
+++ terraform/main.tf
@@ -0,0 +1,45 @@
+provider "aws" {
+  region     = "us-east-1"  # Specify your desired AWS region
+  access_key = var.aws_access_key_id
+  secret_key = var.aws_secret_access_key
+}
+
+resource "aws_instance" "example" {
+  ami                    = "ami-0e731c8a588258d0d" 
+  instance_type          = "t2.micro"
+  key_name               = "demo-project-keys"
+  vpc_security_group_ids = ["sg-0803282e676850cd8"]
+  tags = {
+    Name = "my project"
+
+  }
+  
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum update -y",
+      "mkdir project"
+    ]
+    
+    connection {
+      type        = "ssh"
+      user        = "ec2-user"
+      private_key = var.private_key_base64
+      host        = self.public_ip
+    }
+  }
+}
+
+variable "private_key_base64" {
+  description = "Base64 encoded private key content"
+  type        = string
+}
+
+variable "aws_access_key_id" {
+  description = "AWS access key ID"
+  type        = string
+}
+
+variable "aws_secret_access_key" {
+  description = "AWS secret access key"
+  type        = string
+}
